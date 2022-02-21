23465b66879545e17054da7f0fd3dd46af66accb
Andrey Duka <70843050+0895000@users.noreply.github.com>
2022-02-21 10:23:53 +0200

    fix: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function toReadable (number) {
-  
+    let one = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen',
+            'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];   
+    let ten = ['twenty','thirty', 'forty','fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let n = 0;   
+    if (number < 20) return one[number];   
+    if (number < 100) {     
+        n = number % 10;  
+        return ten[Math.floor(number/10)-2] + (n? " " + one[n]: "");   
+    }
+    if (number < 1000) {    
+         return one[Math.floor(number/100)] + " hundred" + (number % 100 > 0 ?" " + toReadable(number % 100) : "");   
+    } 
 }
